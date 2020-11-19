d9b5a6bc0721ad8a7f9eb821ec273d40018774f3
unknown <daniel-moreno-marquez@hotmail.es>
2020-11-19 18:38:01 +0100

    cambiando ruta app

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get( '/{path?}', function(){
-    return view( 'app' );
-} )->where('path', '.*');
+Route::get('/{path?}', 'Controller@index')->where('path', '.*');
